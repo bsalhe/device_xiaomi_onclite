15f15a12315de478a50240791ff7442c84304781
bsalhe <133101667+bsalhe@users.noreply.github.com>
2023-09-01 23:21:41 +0545

    Update AndroidProducts.mk tp match arrow OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/arrow_onclite.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_onclite-user \
-    lineage_onclite-userdebug \
-    lineage_onclite-eng
+    arrow_onclite-user \
+    arrow_onclite-userdebug \
+    arrow_onclite-eng
